709ecf0d513e913a3a11d468912c587cf8aa8e46
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-06 01:12:51 +0300

    Make adjustments on grid\nGrids had gap between them.

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ createGrid(256);
 function createGrid(size){
     for(i = 0; i < size; i++){
         const gridElement = document.createElement('div');
+        const gridElementBorder = document.createElement('div');
+        gridElementBorder.classList.add('square-grid-border');
         gridElement.classList.add('square-grid');
+        
+        
+        gridElement.appendChild(gridElementBorder);
         container.appendChild(gridElement);
+
         gridElement.addEventListener('mouseover', () => {
             gridElement.style = "background-color: black;"
         })
