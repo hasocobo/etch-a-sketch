972956f80360e983321fb2abb80dacfba02b7c62
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-06 19:53:12 +0300

    Add change grid size feature and complete the project

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
 const wrapper = document.querySelector('.max-width-800');
+const resetButton = document.querySelector('#reset-button');
+const slider = document.querySelector('#slider');
+const changeGridButton = document.querySelector('#grid-change-button');
+
 const container = document.createElement('div');
 container.id = 'grid-container';
 wrapper.appendChild(container);
 
-createGrid(256);
 
-function createGrid(size){
-    for(i = 0; i < size; i++){
-        const gridElement = document.createElement('div');
-        const gridElementBorder = document.createElement('div');
-        gridElementBorder.classList.add('square-grid-border');
-        gridElement.classList.add('square-grid');
-        
-        
-        gridElement.appendChild(gridElementBorder);
-        container.appendChild(gridElement);
+createGrid(16);
 
-        gridElement.addEventListener('mouseover', () => {
-            gridElement.style = "background-color: black;"
-        })
+
+resetButton.addEventListener('click', () => {
+    resetGrid();
+})
+changeGridButton.addEventListener('click', () => {
+    gridSize = prompt('Enter New Grid Size per Row and Column');
+    removeChildren(container);
+    createGrid(gridSize);
+})
+
+
+
+
+function removeChildren(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
-/*
-js ile container oluştur
-square gridleri for döngüsü ile ekle
-container'ın css'ini ayarla
-hover özelliği ekle
+function createGrid(amountPerRowAndColumn){
+    let amount = amountPerRowAndColumn * amountPerRowAndColumn;
+    for(i = 0; i < amount; i++){
+        createSquare(amountPerRowAndColumn);
+    }
+}
 
-*/
+function createSquare(amount){
+    let size = 640 / amount;
+    const gridElement = document.createElement('div');
+    const gridElementBorder = document.createElement('div');
+    gridElement.style = `width: ${size}px; height: ${size}px;`;
+    gridElementBorder.classList.add('square-grid-border');
+    //gridElement.classList.add('square-grid');
+
+    
+    gridElement.appendChild(gridElementBorder);
+    container.appendChild(gridElement);
+
+    gridElement.addEventListener('mouseover', () => {
+        gridElement.classList.add('bg-black');
+    })
+}
+
+function resetGrid() {
+    const elements = document.querySelectorAll('.bg-black');
+    for (let element of elements) {
+        element.classList.remove('bg-black');
+    }
+}
+
